1f7263a64d9bd44332c9489950680ea015750a9a
Flamel-Ferreira <flamelferreira05@hotmail.com>
2022-08-02 20:36:27 -0300

    Padronização de Estilos de Texto

---
+++ Js/ver_mais.js
@@ -0,0 +1,7 @@
+var bnt_ver = document.querySelector(".bnt_area");
+
+console.log(bnt_ver);
+
+bnt_ver.addEventListener("click",function(){
+ // ver uma forma de selecionar os que ter class hidden e tirar para quando for ver menos não esconder todos.
+})
