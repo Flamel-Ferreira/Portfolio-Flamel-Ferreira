2d6d271a547fb652099603fc8516703031fabc57
Flamel-Ferreira <flamelferreira05@hotmail.com>
2022-08-04 17:03:16 -0300

    botao ver mais completo

--- Js/ver_mais.js
+++ Js/ver_mais.js
@@ -1,7 +1,35 @@
 var bnt_ver = document.querySelector(".bnt_area");
+var escondido = document.querySelectorAll(".hidden");
+var boxes_sec4 = document.querySelectorAll(".projetos_boxes_single")
+var ver_mais = document.querySelector(".bnt_area p")
+let area_botao = document.querySelector(".bnt_area")
 
 console.log(bnt_ver);
 
 bnt_ver.addEventListener("click",function(){
+  
+  for(var i = 3; i< boxes_sec4.length; i++){
+    
+    if(boxes_sec4[i].classList.contains("hidden")){
+      boxes_sec4[i].classList.remove("hidden");
+      boxes_sec4[i].classList.add("visible");
+
+      ver_mais.innerHTML = "↑"
+      ver_mais.style.transform =  "translateY(-4px)";
+      ver_mais.style.fontSize = 35;
+      area_botao.style.width = 80;
+
+    }else if(boxes_sec4[i].classList.contains("visible")){
+      boxes_sec4[i].classList.remove("visible");
+      boxes_sec4[i].classList.add("hidden");
+
+      ver_mais.innerHTML = "Ver mais!"
+      ver_mais.style.transform =  "translateY(0)";
+      ver_mais.style.fontSize = 24;
+      area_botao.style.width = 200;
+    }
+  }
+
+  
  // ver uma forma de selecionar os que ter class hidden e tirar para quando for ver menos não esconder todos.
 })
