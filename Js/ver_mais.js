79a2a4b8ab92997e1325f57bdeaee6dfbf4e6987
Flamel-Ferreira <flamelferreira05@hotmail.com>
2022-09-02 16:43:15 -0300

    Diminuir o tamanho do botão ver mais no responsivo

--- Js/ver_mais.js
+++ Js/ver_mais.js
@@ -11,19 +11,35 @@ bnt_ver.addEventListener("click",function(){
       boxes_sec4[i].classList.remove("hidden");
       boxes_sec4[i].classList.add("visible");
 
-      ver_mais.innerHTML = "↑"
-      ver_mais.style.transform =  "translateY(-4px)";
-      ver_mais.style.fontSize = 35;
-      area_botao.style.width = 80;
+      if(window.matchMedia('(max-width:766px)').matches){ //Caso tamanho de tela menor que 766px
+        ver_mais.innerHTML = "↑"
+        ver_mais.style.transform =  "translateY(-4px)";
+        ver_mais.style.fontSize = 28;
+        area_botao.style.width = 70;
+      } else { 
+        ver_mais.innerHTML = "↑"
+        ver_mais.style.transform =  "translateY(-4px)";
+        ver_mais.style.fontSize = 35;
+        area_botao.style.width = 80;
+      }
 
     }else if(boxes_sec4[i].classList.contains("visible")){
       boxes_sec4[i].classList.remove("visible");
       boxes_sec4[i].classList.add("hidden");
 
-      ver_mais.innerHTML = "Ver mais!"
-      ver_mais.style.transform =  "translateY(0)";
-      ver_mais.style.fontSize = 24;
-      area_botao.style.width = 200;
+
+      if(window.matchMedia('(max-width:766px)').matches)
+      {//Caso tamanho de tela menor que 766px
+        ver_mais.innerHTML = "Ver mais!"
+        ver_mais.style.transform =  "translateY(0)";
+        ver_mais.style.fontSize = 18;
+        area_botao.style.width = 125;
+      } else {
+        ver_mais.innerHTML = "Ver mais!"
+        ver_mais.style.transform =  "translateY(0)";
+        ver_mais.style.fontSize = 24;
+        area_botao.style.width = 200;
+      }
     }
   }
 })
