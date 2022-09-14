1ebf5f37e531a157142ed3a961305ed95a92fa12
Flamel-Ferreira <flamelferreira05@hotmail.com>
2022-09-14 12:26:56 -0300

    Preparação para futuras Habilidades

--- Js/scroll_suave.js
+++ Js/scroll_suave.js
@@ -1,12 +1,14 @@
-const alt_inicio = document.querySelector('#inicio').getBoundingClientRect().top
-const alt_sobre_mim = document.querySelector('#sobre_mim').getBoundingClientRect().top
-const alt_habilidades = document.querySelector('#habilidades').getBoundingClientRect().top
-const alt_projetos =  document.querySelector('#projetos').getBoundingClientRect().top
 
 const anc_menu_Array = Array.from(anc_menu);
 
 for( let f = 0; f < anc_menu.length; f++){
   anc_menu[f].addEventListener('click',(t)=>{
+
+    var alt_inicio = document.querySelector('#inicio').getBoundingClientRect().top
+    var alt_sobre_mim = document.querySelector('#sobre_mim').getBoundingClientRect().top
+    var alt_habilidades = document.querySelector('#habilidades').getBoundingClientRect().top
+    var alt_projetos =  document.querySelector('#projetos').getBoundingClientRect().top
+
     var index_menu = anc_menu_Array.findIndex(function(element){return element === t.target})
 
     if(index_menu == 0){
@@ -29,9 +31,9 @@ for( let f = 0; f < anc_menu.length; f++){
     }
     else if(index_menu == 3){
       window.scrollTo({
-        top: alt_projetos - 100,
-        behavior: "smooth"
-      })
+          top: alt_projetos - 100,
+          behavior: "smooth"
+          })
     }
   })
 }
